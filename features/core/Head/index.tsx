33e4e85687be029c790df003c75036ebe53ea831
Vishal Vijay <vishalvijay@users.noreply.github.com>
2021-08-22 23:06:21 +0530

    Update index.tsx

--- features/core/Head/index.tsx
+++ features/core/Head/index.tsx
@@ -16,7 +16,7 @@ const Head: React.FC<HeadProps> = ({ title }) => {
       <title>{newTitle}</title>
       <meta
         name="description"
-        content="Fullstack Engineer - 10x engineer, entrepreneur, and mentor with 7+ years of solid experience building consumer-facing software. Skilled in cloud computing, scaling the infrastructure, and full-stack Node.js/RoR/Android/React. Expert in creating scalable systems from scratch. #rubyonrails #nodejs #reactjs #android #ruby #typescript #javascript #java #kotlin #mysql #postgressql #mongodb #firestore #graphql #html #css #redux #nextjs #drools #firebase #aws #gcp #solr #elasticsearch #redis #expressjs #webcomponent #microservices #grpc #docker #cicd #distributedsystems #bigquery #googleanalytics #cloudcomputing #agilemethodologies #scalability #datavisualization"
+        content="Fullstack Engineer - 10x engineer, entrepreneur, and mentor with 8+ years of solid experience building consumer-facing software. Skilled in cloud computing, scaling the infrastructure, and full-stack Node.js/RoR/Android/React. Expert in creating scalable systems from scratch. #rubyonrails #nodejs #reactjs #android #ruby #typescript #javascript #java #kotlin #mysql #postgressql #mongodb #firestore #graphql #html #css #redux #nextjs #drools #firebase #aws #gcp #solr #elasticsearch #redis #expressjs #webcomponent #microservices #grpc #docker #cicd #distributedsystems #bigquery #googleanalytics #cloudcomputing #agilemethodologies #scalability #datavisualization"
       />
       <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
